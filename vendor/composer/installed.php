83ea87a4082a1b855223c6da57e387a5e8917bbc
YVic95 <vika.yevlentieva@gmail.com>
2022-10-28 16:02:39 +0300

    fixed composer json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '0.1.0-alpha',
+        'version' => '0.1.0.0-alpha',
+        'type' => 'magento2-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'mageplaza/module-star-track',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mageplaza/module-star-track' => array(
+            'pretty_version' => '0.1.0-alpha',
+            'version' => '0.1.0.0-alpha',
+            'type' => 'magento2-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
